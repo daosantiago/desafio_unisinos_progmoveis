307e20fb68d150a1683b9d21c6c9644c4e126ca6
Anderson <daosantiago@gmail.com>
2025-03-29 00:37:16 -0300

    use new components

--- lib/screens/initial_screen.dart
+++ lib/screens/initial_screen.dart
@@ -1,7 +1,5 @@
-import 'package:desafio_unisinos/components/task.dart';
-import 'package:desafio_unisinos/data/task_inherited.dart';
-import 'package:desafio_unisinos/screens/form_screen.dart';
-import 'package:desafio_unisinos/screens/user_form_screen.dart';
+import 'package:desafio_unisinos/components/bottom_menu.dart';
+import 'package:desafio_unisinos/components/menus/main_menu.dart';
 import 'package:desafio_unisinos/theme/app_colors.dart';
 import 'package:flutter/material.dart';
 
@@ -37,10 +35,6 @@ class _InitialScreenState extends State<InitialScreen> {
           ),
           body: Container(
             color: AppColors.background,
-            /*child: ListView(
-              padding: EdgeInsets.only(top: 8, bottom: 70),
-              children: TaskInherited.of(context).taskList,
-            ),*/
             child: Column(
               children: [
                 Container(
@@ -52,99 +46,15 @@ class _InitialScreenState extends State<InitialScreen> {
                 // MainMenu(parentContext: context),
                 MainMenu(),
                 const Spacer(),
-                _BottomMenu(),
+                BottomMenu(),
               ],
             ),
           ),
-          /*floatingActionButton: FloatingActionButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (newContext) => FormScreen(taskContext: context),
-                ),
-              );
-            },
-            child: Icon(Icons.add),
-          ),*/
         ),
       ],
     );
   }
 }
 
-class _MenuButton extends StatelessWidget {
-  final IconData icon;
-  final String label;
-  final VoidCallback onPressed;
 
-  const _MenuButton({
-    required this.icon,
-    required this.label,
-    required this.onPressed,
-  });
 
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
-      child: ElevatedButton(
-        onPressed: onPressed,
-        style: ElevatedButton.styleFrom(
-          backgroundColor: AppColors.mainColor,
-          foregroundColor: Colors.black,
-          minimumSize: const Size(double.infinity, 50),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(30),
-          ),
-        ),
-        child: Row(
-          children: [
-            const SizedBox(width: 40), // margem da esquerda
-            Icon(icon),
-            const SizedBox(width: 20), // espaço entre ícone e texto
-            Expanded(child: Text(label, style: const TextStyle(fontSize: 16))),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class _BottomMenu extends StatelessWidget {
-  const _BottomMenu({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 16),
-      child: Container(
-        margin: const EdgeInsets.symmetric(horizontal: 24),
-        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
-        decoration: BoxDecoration(
-          color: Colors.black87,
-          borderRadius: BorderRadius.circular(16),
-        ),
-      ],
-    );
-  }
-}
-
-class _BottomMenuItem extends StatelessWidget {
-  final IconData icon;
-
-  const _BottomMenuItem({required this.icon});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 48,
-      height: 48,
-      decoration: BoxDecoration(
-        color: AppColors.mainColor,
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: Icon(icon, color: Colors.black),
-    );
-  }
-}
